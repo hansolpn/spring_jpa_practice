40f89c7e7e98f5bc211144331038ce82df646b05
hansol na <142369934+hansolpn@users.noreply.github.com>
2023-11-22 15:11:41 +0900

    231122 종합 연습예제 3 (게시물 등록)

--- src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
+++ src/main/java/com/study/jpa/chap05_practice/api/PostApiController.java
@@ -1,14 +1,17 @@
 package com.study.jpa.chap05_practice.api;
 
 import com.study.jpa.chap05_practice.dto.PageDTO;
+import com.study.jpa.chap05_practice.dto.PostCreateDTO;
+import com.study.jpa.chap05_practice.dto.PostDetailResponseDTO;
 import com.study.jpa.chap05_practice.dto.PostListResponseDTO;
 import com.study.jpa.chap05_practice.service.PostService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class PostApiController {
     /*
         게시물 목록 조회: /posts            - GET, param: (page, size)
         게시물 개별 조회: /posts/{id}       - GET
-        게시물 등록:     /posts            - POST
+        게시물 등록:     /posts            - POST, payload: (title, writer, content, hashTags)
         게시물 수정:     /posts/{id}       - PATCH
         게시물 삭제:     /posts/{id}       - DELETE
      */
@@ -35,6 +38,52 @@ public class PostApiController {
         PostListResponseDTO resDTO = postService.getPosts(dto);
 
         return ResponseEntity.ok().body(resDTO);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable long id) {
+        log.info("/api/v1/posts/{} GET", id);
+
+        try {
+            PostDetailResponseDTO resDTO = postService.getDetail(id);
+            return ResponseEntity.ok().body(resDTO);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
+
+    // 게시물 등록
+    @PostMapping
+    public ResponseEntity<?> create(
+            @Validated @RequestBody PostCreateDTO dto,
+            BindingResult result // 검증 에러 정보를 가진 객체
+            
+    ) {
+        log.info("/api/v1/posts POST - payload: {}", dto);
+
+        if (dto == null) {
+            return ResponseEntity.badRequest().body("등록 게시물 정보를 전달해 주세요!");
+        }
+
+        if (result.hasErrors()) { // 입력값 검증 단계에서 문제가 있었다면 true
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            fieldErrors.forEach(err -> {
+                log.warn("invalid client data - {}", err.toString());
+            });
+
+            return ResponseEntity.badRequest().body(fieldErrors);
+        }
+
+        try {
+            // 위에 존재하는 if문을 모두 건너뜀 -> dto가 null도 아니고, 입력값 검증도 모두 통과함. -> service에게 명령
+            PostDetailResponseDTO resDTO = postService.insert(dto);
+            return ResponseEntity.ok().body(resDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body("미안 서버 터짐 원인: " + e.toString());
+        }
+
     }
 
 }
